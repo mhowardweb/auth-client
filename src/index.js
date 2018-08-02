490cda2153ad10b69014dbf7d847d14209898aef
mhowardweb <mhoward181@outlook.com>
2018-08-02 15:54:34 +0100

    Finish routing

--- src/index.js
+++ src/index.js
@@ -9,10 +9,15 @@ import App from './components/App';
 import Welcome from './components/Welcome';
 import Signup from './components/auth/Signup';
 import reducers from './reducers';
+import Feature from './components/Feature';
+import Signout from './components/auth/Signout';
+import Signin from './components/auth/Signin';
 
 const store = createStore(
-  reducers,
-  {},
+  reducers, 
+  {
+   auth: { authenticated: localStorage.getItem('token') }
+  },
   applyMiddleware(reduxThunk)
 );
 
@@ -22,6 +27,9 @@ ReactDOM.render(
       <App>
         <Route path="/" exact component={Welcome} />
         <Route path="/signup" component={Signup} />
+        <Route path="/feature" component={Feature} />
+        <Route path="/signout" component={Signout} />
+        <Route path="/signin" component={Signin} />
       </App>
     </BrowserRouter>
   </Provider>,
